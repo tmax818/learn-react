db63cb2f69a4ad64c5e0ffeec0c6a302da84f807
tmax818 <tylermaxwell661@gmail.com>
2020-07-26 16:23:16 -0700

    add Main comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
