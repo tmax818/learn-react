fe08ebaed9bb054579d7ea0af2c0c32aa485d74d
tmax818 <tylermaxwell661@gmail.com>
2020-07-26 19:06:05 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
